fa534cb5a72382a471be804b5f24d6d67ab8898d
Askarpour <mehrnoosh.askarpour@gmail.com>
2019-05-30 11:49:08 +0200

    the cleaned-up version of position parser

--- find_new_new.py
+++ find_new_new.py
@@ -1,35 +1,26 @@
 from collections import defaultdict
 
 position = defaultdict(list)
+parts = defaultdict()
+parts = {'BASE_1_IN_L_': 'base', 'LINK1_1_IN_L_': 'link_1','LINK2_1_IN_L_': 'link_2','ENDEFF_1_IN_L_': 'endeff','OPERATOR_1_ARM_AREA_IN_L_': 'arm', 'OPERATOR_1_HEAD_AREA_IN_L_': 'head'}
+#dealing with time = 0
+for p in parts:
+	position[parts[p]].append('0')
+#
 
-
-
-with open('output.hist.txt', 'r') as f_orig, open('values.txt', 'w') as f_new, open('den.txt', 'w') as f_den:
+with open('output.hist.txt', 'r') as f_orig:
+	i = 0
 	for line in f_orig:
-		i=1
-		while (i < 16):
-			if "------ time %d" % (i) in line:
-				f_new.write(line)
-				i=i+1
-				for line in f_orig:
-					if "BASE_1_IN_L_" in line:
-						position['base'].append(line[-2:])
-						print(position['base'])
-					if "LINK1_1_IN_L_" in line:
-						position['link_1'].append(line[-2:])
-						print(position['link_1'])
-					if "LINK2_1_IN_L_" in line:
-						position['link_2'].append(line[-2:])
-						print(position['link_2'])
-					if "ENDEFF_1_IN_L_" in line:
-						position['endeff'].append(line[-2:])
-						print(position['endeff'])
-					if "OPERATOR_1_ARM_AREA_IN_L_" in line:
-						position['arm'].append(line[-2:])
-						print(position['arm'])
-					if "OPERATOR_1_HEAD_AREA_IN_L_" in line:
-						position['head'].append(line[-2:])
-						print(position['head'])
-					if "------ time %d" % (i) in line:
-						break
+		if (i > 1):
+			for p in parts:
+				if p in line:
+					position[parts[p]].append(line[-2:])
+		if "------ time" in line:
+			i += 1
 
+# print(position['link_1'])
+# print(position['link_2'])
+# print(position['base'])
+# print(position['arm'])
+# print(position['head'])
+# print(position['endeff'])
